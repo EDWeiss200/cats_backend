0b6cbcddc324d9f928c6f9abed990d537c3370b1
FeRRRiJane <mykhoedovs@gmail.com>
2024-10-02 12:56:08 +0700

    ch: 11

--- app/main.py
+++ app/main.py
@@ -6,21 +6,21 @@ from contextlib import asynccontextmanager
 from api.cat_router import router as cat_router
 
 from redis import asyncio as aioredis
-from fastapi_cache import FastAPICache
-from fastapi_cache.backends.redis import RedisBackend
+#from fastapi_cache import FastAPICache
+#from fastapi_cache.backends.redis import RedisBackend
 
 import uvicorn
 
-@asynccontextmanager
-async def lifespan(_: FastAPI) -> AsyncIterator[None]:
-    redis = aioredis.from_url("redis://localhost")
-    FastAPICache.init(RedisBackend(redis), prefix="fastapi-cache")
-    yield
+#@asynccontextmanager
+#async def lifespan(_: FastAPI) -> AsyncIterator[None]:
+    #redis = aioredis.from_url("redis://localhost")
+    #FastAPICache.init(RedisBackend(redis), prefix="fastapi-cache")
+    #yield
 
 
 app = FastAPI(
     title="EDWeissTrainingHub",
-    lifespan=lifespan
+    #lifespan=lifespan
 )
 
 app.include_router(cat_router)
